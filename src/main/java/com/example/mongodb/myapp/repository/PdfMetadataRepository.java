1f0f350c1abfb964f5b84e13d110082bf354f75d
서정원 <tjtkdvl@gmail.com>
2025-02-18 10:23:25 +0900

    refact: 추상화된 api에 맞추어 기존 코드 삭제  - block, document 등 특수한 컬렉션에 접근하는 로직 제거

--- src/main/java/com/example/mongodb/myapp/repository/PdfMetadataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mongodb.myapp.repository;
-
-import com.example.mongodb.myapp.model.PdfMetadata;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PdfMetadataRepository extends MongoRepository<PdfMetadata, String> {
-}
